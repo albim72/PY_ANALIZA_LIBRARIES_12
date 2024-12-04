51ddfadbb827ad4f082f14afba6e33f2c7514ee3
albim72 <marcin.albiniak72@gmail.com>
2024-12-04 13:09:06 +0100

    Create main.py

---
+++ moje_moduly/main.py
@@ -0,0 +1,17 @@
+# import dane
+# import dane as df
+from dane import nr_filii as nf
+from dane import book as bk
+from funkcje.bfunkcje import czytaj_liste, czytaj_slownik
+
+
+
+print("___________ DANE ___________")
+
+print(nf)
+print(bk)
+
+print("___________ DANE -> funkcyjnie ___________")
+
+czytaj_liste(nf)
+czytaj_slownik(bk)
